c4600ebe9618ec38741d5e5e38cf6a2b51e7620d
Gunay <160528422+GunayGadimova1357@users.noreply.github.com>
2026-05-01 10:12:31 +0400

    Create myfun.m

---
+++ myfun.m
@@ -0,0 +1,9 @@
+function f = myfun(x)
+% Nümunə 1 üçün funksiyalar dəsti
+% Minimax məsələsinin həlli üçün istifadə olunur
+
+f(1) = 2*x(1)^2 + x(2)^2 - 48*x(1) - 40*x(2) + 304;
+f(2) = -x(1)^2 - 3*x(2)^2;
+f(3) = x(1) + 3*x(2) - 18;
+f(4) = -x(1) - x(2);
+f(5) = x(1) + x(2) - 8;
